815f78f793202630bfa5e7e51042e0e6deea3bbd
mawaisramzan819 <theawaisx.ai@gmail.com>
2026-07-06 17:35:21 +0500

    Adding day 4 files

---
+++ day 4/app.py
@@ -0,0 +1,28 @@
+def advanced_login():
+    try:
+    
+        user_name = input("Enter Your username: ")  # user enter his username 
+        if not user_name:                           # not opertor used for username cannot blank
+            return ("Username cann't be blank")
+        elif len(user_name) > 6:                     #  this will define lenght of almost username
+            return ("Username have at least 6 characters.")
+        
+        password = input("Enter password: ")
+        if len(password) < 8 or len(password) > 20 :
+         return ("Passeord must have 8 to 20 characters.")
+        
+        confirm_password = input("Confirm passeord: ")
+        if password != confirm_password:
+            return ("Password is not same try again!")
+        
+        age = int(input("Enter your age: "))
+        if age >= 18:
+            return ("Login successfull.")
+        else:
+            return ("Please fill the form according to term and conditions.")
+
+    except ValueError:
+        return("Enter valid input!")
+
+
+print(advanced_login())
